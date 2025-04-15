fd67c27c3bfe7ec4ae5dca3594c5dd2683b0a395
K93wyuta <k93wyuta@gmail.com>
2025-04-15 14:52:51 +0900

    調整

--- assets/scripts/common.js
+++ assets/scripts/common.js
@@ -1,21 +1,13 @@
-let pathDepth;
+// ファイル階層をカウント
+const pathDepth = location.pathname.replace(/\/$/, '')
+  .split('/')
+  .filter(segment => segment && !segment.includes('.html'))
+  .length - 1;
 
-if (location.protocol === 'file:') {
-  // ローカル環境：file:// のとき
-  const localPath = location.pathname.replace(/\\/g, '/'); // Windows対策
-  const parts = localPath.split('/');
-  // プロジェクトルートを "common" の1つ上の階層とみなす
-  const index = parts.lastIndexOf('common');
-  pathDepth = index > 0 ? parts.length - index - 2 : parts.length - 2;
-} else {
-  // サーバーやGitHub Pages上
-  pathDepth = location.pathname.replace(/\/$/, '')
-    .split('/')
-    .filter(segment => segment && !segment.includes('.html'))
-    .length - 1;
-}
-
+// 最低値は 0 にする
 const safeDepth = Math.max(0, pathDepth);
+
+// ベースパスを生成
 const basePath = '../'.repeat(safeDepth);
 
 // ヘッダーを挿入する関数
