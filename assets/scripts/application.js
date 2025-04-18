25e3e9bddeda90fb3700e376019515124eb4d46f
K93wyuta <k93wyuta@gmail.com>
2025-04-18 17:51:53 +0900

    調整

--- assets/scripts/application.js
+++ assets/scripts/application.js
@@ -6,7 +6,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const name = form.name.value.trim();
     const email = form.email.value.trim();
     const phone = form.phone.value.trim();
-    const message = form.message.value.trim();
 
     // メールアドレスのバリデーション
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
